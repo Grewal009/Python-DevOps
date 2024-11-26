07de73a35f40d6fd2353abdc335d171c7ce36786
Aneel Grewal <aneel.grewal@hotmail.com>
2024-11-26 23:09:03 +0100

    Day-05 reading command line arguments

---
+++ Day-05/calculate.py
@@ -0,0 +1,17 @@
+import sys
+
+def add(num1,numm2):
+    res = num1 + numm2
+    return res
+
+def mul(num1, num2):
+    res = num1 * num2
+    return res
+
+num1 = float(sys.argv[1])
+operation = sys.argv[2]
+num2 = float(sys.argv[3])
+
+if operation == "add":
+    output = add(num1,num2)
+    print(output)
